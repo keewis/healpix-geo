12ed184bf98b682c618c9cad4c071f115bbf39a2
Justus Magin <keewis@posteo.de>
2025-11-27 14:30:57 +0100

    expose the bilinear interpolation function

--- src/indexing_schemes/nested/mod.rs
+++ src/indexing_schemes/nested/mod.rs
@@ -4,7 +4,7 @@ pub(crate) mod hierarchy;
 mod sets;
 
 pub(crate) use self::coordinates::{
-    angular_distances, healpix_to_lonlat, lonlat_to_healpix, vertices,
+    angular_distances, bilinear_interpolation, healpix_to_lonlat, lonlat_to_healpix, vertices,
 };
 pub(crate) use self::coverage::{
     box_coverage, cone_coverage, elliptical_cone_coverage, polygon_coverage, zone_coverage,
