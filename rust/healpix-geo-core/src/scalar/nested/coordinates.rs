380136c556e3db68e8a1231ee44f70f1f76a9026
Justus Magin <keewis@users.noreply.github.com>
2026-03-30 14:24:09 +0200

    normalize longitudes to 0 to 360 degrees (#166)
    * test that we get the expected level 0 cells
    
    * make sure we're within a range of [0, 360)

--- rust/healpix-geo-core/src/scalar/nested/coordinates.rs
+++ rust/healpix-geo-core/src/scalar/nested/coordinates.rs
@@ -5,7 +5,7 @@ use cdshealpix::nested::Layer;
 pub fn healpix_to_lonlat(hash: &u64, layer: &Layer, ellipsoid: &Ellipsoid) -> (f64, f64) {
     let center = layer.center(*hash);
 
-    let lon = center.0.to_degrees();
+    let lon = center.0.to_degrees().rem_euclid(360.0);
     let lat = ellipsoid
         .latitude_authalic_to_geographic(center.1)
         .to_degrees();
@@ -14,7 +14,7 @@ pub fn healpix_to_lonlat(hash: &u64, layer: &Layer, ellipsoid: &Ellipsoid) -> (f
 }
 
 pub fn lonlat_to_healpix(lon: &f64, lat: &f64, layer: &Layer, ellipsoid: &Ellipsoid) -> u64 {
-    let lon_ = lon.to_radians();
+    let lon_ = lon.rem_euclid(360.0).to_radians();
     let lat_ = ellipsoid.latitude_geographic_to_authalic(lat.to_radians());
 
     layer.hash(lon_, lat_)
@@ -32,4 +32,44 @@ pub fn vertices(hash: &u64, layer: &Layer, ellipsoid: &Ellipsoid) -> Vec<(f64, f
             )
         })
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ellipsoid::ReferenceEllipsoid;
+    use cdshealpix as healpix;
+    use geodesy::ellps::Ellipsoid as GeodesyEllipsoid;
+
+    #[test]
+    fn test_lonlat_to_healpix_edge_cases_lon() {
+        let layer = healpix::nested::get(0);
+        let ellipsoid = Ellipsoid::Ellipsoid(ReferenceEllipsoid::new(
+            GeodesyEllipsoid::named("WGS84").unwrap(),
+        ));
+
+        let lon: f64 = -180.0;
+        let lat: f64 = 75.0;
+
+        let actual = lonlat_to_healpix(&lon, &lat, layer, &ellipsoid);
+        assert_eq!(actual, 2);
+
+        let lon: f64 = 180.0;
+        let lat: f64 = 75.0;
+
+        let actual = lonlat_to_healpix(&lon, &lat, layer, &ellipsoid);
+        assert_eq!(actual, 2);
+
+        let lon: f64 = 0.0;
+        let lat: f64 = 75.0;
+
+        let actual = lonlat_to_healpix(&lon, &lat, layer, &ellipsoid);
+        assert_eq!(actual, 0);
+
+        let lon: f64 = 360.0;
+        let lat: f64 = 75.0;
+
+        let actual = lonlat_to_healpix(&lon, &lat, layer, &ellipsoid);
+        assert_eq!(actual, 0);
+    }
 }
--- rust/healpix-geo-core/src/scalar/ring/coordinates.rs
+++ rust/healpix-geo-core/src/scalar/ring/coordinates.rs
@@ -5,7 +5,7 @@ use cdshealpix as healpix;
 pub fn healpix_to_lonlat(hash: &u64, nside: &u32, ellipsoid: &Ellipsoid) -> (f64, f64) {
     let center = healpix::ring::center(*nside, *hash);
 
-    let lon = center.0.to_degrees();
+    let lon = center.0.to_degrees().rem_euclid(360.0);
     let lat = ellipsoid
         .latitude_authalic_to_geographic(center.1)
         .to_degrees();
@@ -14,7 +14,7 @@ pub fn healpix_to_lonlat(hash: &u64, nside: &u32, ellipsoid: &Ellipsoid) -> (f64
 }
 
 pub fn lonlat_to_healpix(lon: &f64, lat: &f64, nside: &u32, ellipsoid: &Ellipsoid) -> u64 {
-    let lon_ = lon.to_radians();
+    let lon_ = lon.rem_euclid(360.0).to_radians();
     let lat_ = ellipsoid.latitude_geographic_to_authalic(lat.to_radians());
 
     healpix::ring::hash(*nside, lon_, lat_)
@@ -32,4 +32,44 @@ pub fn vertices(hash: &u64, nside: &u32, ellipsoid: &Ellipsoid) -> Vec<(f64, f64
             )
         })
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ellipsoid::ReferenceEllipsoid;
+    use cdshealpix as healpix;
+    use geodesy::ellps::Ellipsoid as GeodesyEllipsoid;
+
+    #[test]
+    fn test_lonlat_to_healpix_edge_cases_lon() {
+        let nside = healpix::nside(0);
+        let ellipsoid = Ellipsoid::Ellipsoid(ReferenceEllipsoid::new(
+            GeodesyEllipsoid::named("WGS84").unwrap(),
+        ));
+
+        let lon: f64 = -180.0;
+        let lat: f64 = 75.0;
+
+        let actual = lonlat_to_healpix(&lon, &lat, &nside, &ellipsoid);
+        assert_eq!(actual, 2);
+
+        let lon: f64 = 180.0;
+        let lat: f64 = 75.0;
+
+        let actual = lonlat_to_healpix(&lon, &lat, &nside, &ellipsoid);
+        assert_eq!(actual, 2);
+
+        let lon: f64 = 0.0;
+        let lat: f64 = 75.0;
+
+        let actual = lonlat_to_healpix(&lon, &lat, &nside, &ellipsoid);
+        assert_eq!(actual, 0);
+
+        let lon: f64 = 360.0;
+        let lat: f64 = 75.0;
+
+        let actual = lonlat_to_healpix(&lon, &lat, &nside, &ellipsoid);
+        assert_eq!(actual, 0);
+    }
 }
