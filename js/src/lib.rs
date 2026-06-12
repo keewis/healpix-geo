c55aed11c0af41672d9f4ac5d8fcc6a6f5e8b0c9
Justus Magin <keewis@posteo.de>
2026-06-12 15:51:41 +0200

    export ring and zuniq, as well [skip-rtd]

--- js/src/lib.rs
+++ js/src/lib.rs
@@ -6,14 +6,24 @@ pub mod nested;
 pub mod ring;
 pub mod zuniq;
 
+use wasm_bindgen::prelude::*;
+
 pub use crate::coordinates::Coordinate;
 pub use crate::ellipsoid::Ellipsoid;
+
 pub use crate::nested::{
     healpix_to_lonlat as healpix_to_lonlat_nested, lonlat_to_healpix as lonlat_to_healpix_nested,
     vertex as vertex_nested,
 };
 
-use wasm_bindgen::prelude::*;
+pub use crate::ring::{
+    healpix_to_lonlat as healpix_to_lonlat_ring, lonlat_to_healpix as lonlat_to_healpix_ring,
+    vertex as vertex_ring,
+};
+pub use crate::zuniq::{
+    healpix_to_lonlat as healpix_to_lonlat_zuniq, lonlat_to_healpix as lonlat_to_healpix_zuniq,
+    vertex as vertex_zuniq,
+};
 
 #[wasm_bindgen(start)]
 pub fn init_panic_hook() {
