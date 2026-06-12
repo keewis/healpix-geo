42bbf1054bf2f2ed23a5ee92aff3be0e4f273cd0
Justus Magin <keewis@posteo.de>
2026-06-12 14:38:53 +0200

    explicitly import the nested functions [skip-rtd]

--- js/src/lib.rs
+++ js/src/lib.rs
@@ -15,3 +15,7 @@ use wasm_bindgen::prelude::*;
 pub fn init_panic_hook() {
     console_error_panic_hook::set_once();
 }
+
+pub mod nested_ {
+    use crate::nested::{healpix_to_latlon, lonlat_to_healpix, vertex};
+}
