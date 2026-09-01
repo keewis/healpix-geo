98520ac69e634c7e5bf306d3c897c737139e4d7b
Justus Magin <keewis@users.noreply.github.com>
2026-09-01 19:39:59 +0200

    never skip on main or on tags (#279)
    * never skip on main or on tags
    
    * [skip-ci][skip-rtd]

--- ci/skip_rtd.sh
+++ ci/skip_rtd.sh
@@ -2,6 +2,11 @@
 
 CODE_SKIP=183
 
+# never skip on main or on tags
+current_branch=$(git branch --show-current)
+
+([ "$current_branch" == "main" ] || git describe --tags --exact-match 2>/dev/null >/dev/null) && exit 0
+
 n_modifying_commits=$(git --no-pager log --pretty="tformat:%s" main..HEAD -- .readthedocs.yml Cargo.toml Cargo.lock ci/rattler-recipe healpix-geo healpix-geo-python | wc -l)
 most_recent_commit_msg=$(git --no-pager log --pretty="tformat:%s" -1)
 
